3746a675e8d934a3bedd1be8e942d250265217b0
caibird <caibird@caibird.com>
2017-05-07 14:22:41 +0800

    register

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,28 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required, Length, Email
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+                    ValidationError
+from wtforms.validators import Required, Length, Email, Regexp, EqualTo
+from ..models import User
 
 class LoginForm(FlaskForm):
-    email = StringField("邮箱", validators = [Required(), Length(1, 64), Email()])
-    password = PasswordField("密码", validators = [Required()])
+    email = StringField("邮箱:", validators = [Required(), Length(1, 64), Email()])
+    password = PasswordField("密码:", validators = [Required()])
     remember_me = BooleanField("保持登陆")
     submit = SubmitField("提交")
+    
+class RegistrationForm(FlaskForm):
+    email = StringField("邮箱：", validators=[Required(), Length(1, 64), Email()])  
+    username = StringField("用户名：", validators=[Required(), Length(1, 64), \
+                Regexp("^[A-Za-z][A-Za-z0-9_.]*$", 0, "用户名必须只由字母、数字、点或"
+                                                      "下划线组成")])
+    password = PasswordField("密码：", validators=[Required()])
+    password2 = PasswordField("再次输入密码", validators=[Required(), \
+                                            EqualTo("password", "两次密码需要一致")])
+    submit = SubmitField("注册")
+    
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            raise ValidationError("邮箱已被注册，请尝试其他邮箱")
+        
+    
+        
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,10 @@
 from flask import render_template, redirect, request, url_for, flash
 from . import auth
-from .forms import LoginForm
-from flask_login import login_user, login_required, logout_user
+from .forms import LoginForm, RegistrationForm
+from flask_login import login_user, login_required, logout_user, current_user
 from ..models import User
+from .. import db
+from ..email import send_email
 
 
 @auth.route("/login", methods=["GET", "POST"])
@@ -22,3 +24,18 @@ def logout():
     logout_user()
     flash("你已顺利退出.")
     return redirect(url_for("main.index"))
+
+@auth.route("/register", methods=["GET", "POST"])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username = form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        flash("您已成功注册.")
+        return redirect(url_for("auth.login"))
+    return render_template("auth/register.html", form=form)
+
+
+
