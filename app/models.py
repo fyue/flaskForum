212d7559cae3f195c4b66f0530c7990c48c5a0b2
caibird <caibird@caibird.com>
2017-05-11 18:27:17 +0800

    avartar_hash

--- app/models.py
+++ app/models.py
@@ -2,8 +2,10 @@ from . import db, login_manager
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, AnonymousUserMixin
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
-from flask import current_app
+from flask import current_app, request
 from datetime import datetime
+from random import choice, seed
+import hashlib
 
 class Permissions:
     FOLLOW = 0x01
@@ -59,6 +61,7 @@ class User(UserMixin, db.Model):
     about_me = db.Column(db.Text())
     member_since = db.Column(db.DateTime(), default = datetime.utcnow)#register date
     last_seen = db.Column(db.DateTime(), default = datetime.utcnow)
+    avatar_hash = db.Column(db.String(32))
 
     """init Role for users"""
     def __init__(self, **kwargs):
@@ -68,7 +71,9 @@ class User(UserMixin, db.Model):
                 self.role = Role.query.filter_by(permissions = 0xff).first()
             if self.role is None:
                 self.role = Role.query.filter_by(default = True).first()
-    
+        if self.email is not None and self.avatar_hash is None:
+            self.avatar_hash = hashlib.md5(self.email.encode("utf-8")).hexdigest()
+        
     """Password and Verify"""
     @property
     def password(self):
@@ -134,6 +139,7 @@ class User(UserMixin, db.Model):
         if self.query.filter_by(email = new_email).first() is not None:
             return False
         self.email = new_email
+        self.avatar_hash = hashlib.md5(self.email.encode("utf-8")).hexdigest()
         db.session.add(self)
         return True
     
@@ -149,6 +155,18 @@ class User(UserMixin, db.Model):
     def ping(self):
         self.last_seen = datetime.utcnow()
         db.session.add(self)
+
+    def gravatar(self, size = 100, rating = "g"):
+        if request.is_secure:
+            url = "https://secure.gravatar.com/avatar"
+        else:
+            url = "http://www.gravatar.com/avatar"
+        hash = self.avatar_hash or hashlib.md5(self.email.encode("utf-8")).hexdigest()
+        seed(self.id)
+        default = choice(["identicon", "mm", "wavatar", "retro", "monsterid"])
+        return "{url}/{hash}?s={size}&d={default}&r={rating}".format(url = url,
+                 hash = hash, size = size, default = default, rating = rating)
+
 
     def __repr__(self):
         return "<User %r>" %(self.username)
