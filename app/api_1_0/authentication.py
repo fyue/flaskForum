2a36ae4d4ac0b59f8c4e8f251f2251c6ce2048b1
caibird <caibird@caibird.com>
2017-05-28 15:47:01 +0800

    Api Support

---
+++ app/api_1_0/authentication.py
@@ -0,0 +1,42 @@
+from flask import jsonify, g
+from flask_httpauth import HTTPBasicAuth
+from ..models import AnonymousUser, User
+from . import api
+from .errors import forbidden, unauthorized
+
+auth = HTTPBasicAuth()
+
+@auth.verify_password
+def verify_password(email_or_token, password):
+    if email_or_token == "":
+        g.current_user = AnonymousUser()
+        return True
+    if password == "":
+        g.current_user = User.verify_auth_token(email_or_token)
+        g.token_used = True
+        return g.current_user is not None
+    user = User.query.filter_by(email = email_or_token).first()
+    if not user:
+        return False
+    g.current_user = user
+    g.token_used = False
+    return user.verify_password(password)
+
+@auth.error_handler #auth is instance of HTTPBasicAuth()
+def auth_error():
+    return unauthorized("认证失败")
+
+@api.before_request
+@auth.login_required
+def before_request():
+    if not g.current_user.is_anonymous and \
+            not g.current_user.confirmed:
+        return forbidden("帐号未确认")
+        
+@api.route("/token")
+def get_token():
+    if g.current_user.is_anonymous or g.token_used:
+        return unauthorized("认证失败")
+    return jsonify({"token": g.current_user.generate_auth_token(
+                  expiration = 3600), "expiration": 3600})
+
---
+++ app/api_1_0/posts.py
@@ -0,0 +1,52 @@
+from flask import jsonify, request, current_app, g, abort, url_for
+from ..models import Post, Permissions
+from .decorators import permission_required
+from .errors import forbidden
+from .. import db
+from . import api
+
+@api.route("/posts/")
+def get_posts():
+    page = request.args.get("page", 1, type = int)
+    pagination = Post.query.paginate(
+                 page, per_page = current_app.config["FLASKY_POSTS_PER_PAGE"],
+                 error_out = False)
+    posts = pagination.items
+    prev = None
+    if pagination.has_prev:
+        prev = url_for("api.get_posts", page = page - 1, _external = True)
+    next = None
+    if pagination.has_next:
+        next = url_for("api.get_posts", page = page + 1, _external = True)
+    return jsonify({
+        "posts": [post.to_json() for post in posts],
+        "prev": prev,
+        "next": next,
+        "count": pagination.total         
+    })
+
+@api.route("/posts/<int:id>")
+def get_post(id):
+    post = Post.query.get_or_404(id)
+    return jsonify(post.to_json())
+
+@api.route("/posts/", methods = ["POST"])
+@permission_required(Permissions.WRITE_ARTICLES)
+def new_post():
+    post = Post.from_json(request.json)
+    post.author = g.current_user
+    db.session.add(post)
+    db.session.commit()
+    return jsonify(post.to_json()), 201, \
+           {"Location": url_for("api.get_post", id = post.id, _external = True)}
+           
+@api.route("/posts/<int:id>", methods = ["PUT"])
+@permission_required(Permissions.WRITE_ARTICLES)
+def edit_post(id):
+    post = Post.query.get_or_404(id)
+    if g.current_user != post.author and \
+            not g.current_user.can(Permissions.ADMINISTER):
+        return forbidden("权限不足")
+    post.body = request.json.get("body", post.body)
+    db.session.add(post)
+    return jsonify(post.to_json())
