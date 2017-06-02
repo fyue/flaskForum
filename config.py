b91e76b7e84baf5d86ddb03b020ad2ff40fdbea6
caibird <caibird@caibird.com>
2017-06-02 20:21:22 +0800

    Setup

--- config.py
+++ config.py
@@ -8,14 +8,15 @@ class Config:
     SQLALCHEMY_COMMIT_ON_TEARDOWN = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_RECORD_QUERIES = True
-    MAIL_SERVER = "smtp-mail.outlook.com"
-    MAIL_PORT = 587
-    MAIL_USE_TLS = True
-    MAIL_USE_SSL = False
+    SSL_DISABLE = False
+    MAIL_SERVER = "smtp.qq.com"
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     FLASKY_MAIL_SUBJECT_PREFIX = "[Flask forum]"
-    FLASKY_MAIL_SENDER = "Flask Forum Admin<ggggg544648788@outlook.com>"
+    FLASKY_MAIL_SENDER = "Flask Forum Admin<544648788@qq.com>"
     FLASKY_ADMIN = os.environ.get("FLASKY_ADMIN")
     FLASKY_POSTS_PER_PAGE = os.environ.get("FLASKY_POSTS_PER_PAGE") or 25
     FLASKY_FOLLOWERS_PER_PAGE = os.environ.get("FLASKY_FOLLOWERS_PER_PAGE") or 50
@@ -31,18 +32,6 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL") or \
     "mysql://root:ryqbwzy@localhost:3306/test_db"
     
-class DevelopmentConfigQQ(Config):
-    DEBUG = True
-    MAIL_SERVER = "smtp.qq.com"
-    MAIL_PORT = 465
-    MAIL_USE_TLS = False
-    MAIL_USE_SSL = True
-    MAIL_USERNAME = os.environ.get("MAIL_USERNAME_QQ")
-    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD_QQ")
-    FLASKY_MAIL_SENDER = "Flask Forum Admin<544648788@qq.com>"
-    FLASKY_ADMIN = os.environ.get("FLASKY_ADMIN_QQ")
-    SQLALCHEMY_DATABASE_URI = os.environ.get("DEV_DATABASE_URL") or \
-    "mysql://root:ryqbwzy@localhost:3306/test_db"
     
 class TestingConfig(Config):
     TESTING = True
@@ -77,11 +66,31 @@ class ProductionConfig(Config):
         mail_hander.setLevel(logging.ERROR)
         app.logger.addHandler(mail_hander)
     
+class HerokuConfig(ProductionConfig):
+    SSL_DISABLE = bool(os.environ.get('SSL_DISABLE'))
+
+    @classmethod
+    def init_app(cls, app):
+        ProductionConfig.init_app(app)
+
+        """handle proxy server headers"""
+        from werkzeug.contrib.fixers import ProxyFix
+        app.wsgi_app = ProxyFix(app.wsgi_app)
+
+        """log to stderr"""
+        import logging
+        from logging import StreamHandler
+        file_handler = StreamHandler()
+        file_handler.setLevel(logging.WARNING)
+        app.logger.addHandler(file_handler)
+
+    
 config = {
     "development": DevelopmentConfig,
-    "developmentQQ":DevelopmentConfigQQ,
     "testing" : TestingConfig,
     "production" : ProductionConfig,
+    'heroku': HerokuConfig,
+    
     "default" : DevelopmentConfig
 }
     
